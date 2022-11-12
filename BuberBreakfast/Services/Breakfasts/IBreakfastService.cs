4379a168584d119821e73b89ee0fd232fd3ec90d
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-11-13 01:02:26 +0530

    Added Breakfast Interface & Service to implement interface methods

---
+++ BuberBreakfast/Services/Breakfasts/IBreakfastService.cs
@@ -0,0 +1,8 @@
+using BuberBreakfast.Models;
+
+namespace BuberBreakfast.Services.Breakfasts;
+
+public interface IBreakfastService {
+    void CreateBreakfast(Breakfast breakfast);
+    Breakfast GetBreakfast(Guid id);
+}
