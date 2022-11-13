d7643b5d1f9135e46bb994c310b98d25e9c05de1
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-11-13 20:49:14 +0530

    Added logic to return multiple errors in response

--- BuberBreakfast/Controllers/ApiController.cs
+++ BuberBreakfast/Controllers/ApiController.cs
@@ -1,13 +1,32 @@
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BuberBreakfast.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ApiController : ControllerBase {
+public class ApiController : ControllerBase
+{
     protected IActionResult Problem(List<Error> errors)
     {
+        if (errors.All(e => e.Type == ErrorType.Validation))
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+
+            foreach (var error in errors)
+            {
+                modelStateDictionary.AddModelError(error.Code, error.Description);
+            }
+
+            return ValidationProblem(modelStateDictionary);
+        }
+
+        if (errors.Any(e => e.Type == ErrorType.Unexpected))
+        {
+            return Problem();
+        }
+
         var firstError = errors[0];
 
         var statusCode = firstError.Type switch
